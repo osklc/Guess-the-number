1ce10c3510b42c34640cb9ff507add83d22e1883
osklc <osklcmail@gmail.com>
2025-10-27 14:29:15 +0300

    add attempt count

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ int main()
 	int num_real;
 	int num_guessed;
 	int i = 1;
+	int attempt_count;
 	
 	num_real=rand()%100;
 	
@@ -19,7 +20,8 @@ int main()
 		
 		if(num_guessed==num_real)
 		{
-			printf("\x1b[32mYou answered correctly, congratulations.\x1b[0m");
+			printf("\x1b[32mYou answered correctly, congratulations.\x1b[0m\n");
+			printf("\x1b[34mYour attempt count: %d\x1b[0m",attempt_count);
 			i = 0;
 		}
 		
@@ -27,12 +29,14 @@ int main()
 		{
 			printf("\x1b[31mTry again with a smaller number.\x1b[0m\n");
 			i=1;
+			attempt_count = ++attempt_count;
 		}
 		
 		else
 		{
 			printf("\x1b[33mTry again with a larger number.\x1b[0m\n");
 			i=1;
+			attempt_count = ++attempt_count;
 		}
 		
 	}while(i);
